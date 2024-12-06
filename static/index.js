faf476c8b993bae1cdbc53fe33fe12a57ab28b2a
dennisimoo <161797777+dennisimoo@users.noreply.github.com>
2024-12-06 12:12:24 -0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -5,31 +5,6 @@
  *  https://theannoyingsite.com
  *
  *  Author:
- *    Feross Aboukhadijeh
- *    https://feross.org
- *
- *  Patreon:
- *    If you enjoyed this, please support me on Patreon!
- *    https://www.patreon.com/feross
- */
-
-const SCREEN_WIDTH = window.screen.availWidth
-const SCREEN_HEIGHT = window.screen.availHeight
-const WIN_WIDTH = 480
-const WIN_HEIGHT = 360
-const VELOCITY = 15
-const MARGIN = 15
-const TOP_MARGIN = 50
-const TICK_LENGTH = 50
-
-const HIDDEN_STYLE = 'position: fixed; width: 1px; height: 1px; overflow: hidden; top: -10px; left: -10px;'
-
-const ART = [
-  `
-┊┊ ☆┊┊┊┊☆┊┊☆ ┊┊┊┊┊
-┈┈┈┈╭━━━━━━╮┊☆ ┊┊
-┈☆ ┈┈┃╳╳╳▕╲▂▂╱▏┊┊
-┈┈☆ ┈┃╳╳╳▕▏▍▕▍▏┊┊
 ┈┈╰━┫╳╳╳▕▏╰┻╯▏┊┊
 ☆ ┈┈┈┃╳╳╳╳╲▂▂╱┊┊┊
 ┊┊☆┊╰┳┳━━┳┳╯┊ ┊ ☆┊
@@ -62,75 +37,10 @@ const SEARCHES = [
   'why does my belly button smell',
   'why does my cat attack me',
   'why does my dog eat poop',
-  'why does my fart smell so bad',
-  'why does my mom hate me',
-  'why does my pee smell bad',
-  'why does my poop float',
-  'proof that the earth is flat'
-]
-
-const VIDEOS = [
-  'albundy.mp4',
-  'badger.mp4',
-  'cat.mp4',
-  'hasan.mp4',
-  'heman.mp4',
-  'jozin.mp4',
-  'nyan.mp4',
-  'rickroll.mp4',
-  'space.mp4',
-  'trolol.mp4'
-]
-
-const FILE_DOWNLOADS = [
-  'cat-blue-eyes.jpg',
-  'cat-ceiling.jpg',
+  'why does my fart bbb. 
   'cat-crosseyes.jpg',
-  'cat-cute.jpg',
-  'cat-hover.jpg',
-  'cat-marshmellows.jpg',
-  'cat-small-face.jpg',
-  'cat-smirk.jpg',
-  'patreon.png'
-]
-
-const PHRASES = [
-  'The wheels on the bus go round and round, round and round, round and round. The wheels on the bus go round and round, all through the town!',
-  'Dibidi ba didi dou dou, Di ba didi dou, Didi didldildidldidl houdihoudi dey dou',
-  'I like fuzzy kittycats, warm eyes, and pretending household appliances have feelings',
-  'I\'ve never seen the inside of my own mouth because it scares me to death.',
-  'hee haw hee haw hee haw hee haw hee haw hee haw hee haw hee haw hee haw hee haw hee haw',
-  'abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz',
-  'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaak',
-  'eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo eyo'
-]
-
-const LOGOUT_SITES = {
-  AOL: ['GET', 'https://my.screenname.aol.com/_cqr/logout/mcLogout.psp?sitedomain=startpage.aol.com&authLev=0&lang=en&locale=us'],
-  'AOL 2': ['GET', 'https://api.screenname.aol.com/auth/logout?state=snslogout&r=' + Math.random()],
-  Amazon: ['GET', 'https://www.amazon.com/gp/flex/sign-out.html?action=sign-out'],
-  Blogger: ['GET', 'https://www.blogger.com/logout.g'],
-  Delicious: ['GET', 'https://www.delicious.com/logout'], // works!
-  DeviantART: ['POST', 'https://www.deviantart.com/users/logout'],
-  DreamHost: ['GET', 'https://panel.dreamhost.com/index.cgi?Nscmd=Nlogout'],
-  Dropbox: ['GET', 'https://www.dropbox.com/logout'],
-  eBay: ['GET', 'https://signin.ebay.com/ws/eBayISAPI.dll?SignIn'],
-  Gandi: ['GET', 'https://www.gandi.net/login/out'],
-  GitHub: ['GET', 'https://github.com/logout'],
-  GMail: ['GET', 'https://mail.google.com/mail/?logout'],
-  Google: ['GET', 'https://www.google.com/accounts/Logout'], // works!
-  Hulu: ['GET', 'https://secure.hulu.com/logout'],
-  Instapaper: ['GET', 'https://www.instapaper.com/user/logout'],
-  Linode: ['GET', 'https://manager.linode.com/session/logout'],
-  LiveJournal: ['POST', 'https://www.livejournal.com/logout.bml', { 'action:killall': '1' }],
-  MySpace: ['GET', 'https://www.myspace.com/index.cfm?fuseaction=signout'],
-  NetFlix: ['GET', 'https://www.netflix.com/Logout'],
-  'New York Times': ['GET', 'https://www.nytimes.com/logout'],
-  Newegg: ['GET', 'https://secure.newegg.com/NewMyAccount/AccountLogout.aspx'],
-  Photobucket: ['GET', 'https://photobucket.com/logout'],
-  Skype: ['GET', 'https://secure.skype.com/account/logout'],
-  Slashdot: ['GET', 'https://slashdot.org/my/logout'],
-  SoundCloud: ['GET', 'https://soundcloud.com/logout'],
+  'cat-://www.blogger.com/logout.g'],
+  Delicious: ['s://soundcloud.com/logout'],
   'Steam Community': ['GET', 'https://steamcommunity.com/?action=doLogout'],
   'Steam Store': ['GET', 'https://store.steampowered.com/logout/'],
   ThinkGeek: ['GET', 'https://www.thinkgeek.com/brain/account/login.cgi?a=lo'],
@@ -167,7 +77,7 @@ let numSuperLogoutIframes = 0
  * is set) *AND* that parent is a window on the same origin (i.e. the window was
  * opened by us, not an external website)
  */
-const isChildWindow = (window.opener && isParentSameOrigin()) ||
+const isC = (window.opener && isParentSameOrigin()) ||
   window.location.search.indexOf('child=true') !== -1
 
 /**
